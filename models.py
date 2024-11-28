25050c70bcb69ccaad051ef7cad3a49ec43e68a9
seonhu <seonhu0810@gmail.com>
2024-11-28 10:23:55 +0900

    models.py ë데이터베이스 모델 정의

---
+++ models.py
@@ -0,0 +1,12 @@
+from enum import unique
+
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
