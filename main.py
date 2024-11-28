507fe62b4fc7836926414c1819784cc7360749df
seonhu <seonhu0810@gmail.com>
2024-11-28 11:21:34 +0900

    ãmain.py에 userí엔드포인트 등록

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from sys import prefix
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from views import router as user_router
+from database import Base, engine
 
 app = FastAPI()
 
@@ -15,6 +19,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 데이터베이스 테이블 생성
+Base.metadata.create_all(bind=engine)
+
+# 라우터 등록
+app.include_router(user_router, prefix="/api/users")
 
 @app.get("/hello")
 def hello():
