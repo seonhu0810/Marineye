5b24c7f4ecfa1636ea9afb8cc0383ee6290d75d3
seonhu <seonhu0810@gmail.com>
2024-11-28 10:01:21 +0900

    SQlite ë 데이터베이스 설정

--- database.py
+++ database.py
@@ -1,1 +1,16 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+# SQLite 데이터베이스 URL
+DATABASE_URL = "sqlite:///.marineye.db"
+
+# SQLAlchemy 엔진 생성
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
+
+# 세션 생성
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 데이터베이스 모델 베이스 클래스
+Base = declarative_base()
+
