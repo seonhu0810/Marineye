55a22af6622d7268ef29187c9005e3be79e9a966
capstonn <suhyun6655@gmail.com>
2024-12-10 17:27:17 +0900

    Fix line endings to CRLF

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vn91vkjvbo"
+    "revision": "0.4rk1hb1602"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
