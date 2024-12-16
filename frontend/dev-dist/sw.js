dfe73d1ca555ad8080f0a36c4e5969605a69d01a
capstonn <suhyun6655@gmail.com>
2024-12-16 12:21:29 +0900

    service-worker 캐시 내용 수정

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rm847hu22n8"
+    "revision": "0.vl8jc24coq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
